95bc221b675cf7c83ea785a5d5ff56596dcd2cce
R-abodyak <rawanabodiak12@gmail.com>
2022-02-23 08:41:40 +0200

    feat: constructor and push method

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 ﻿class myStack <T> : List<T> {
 private T data ;
+private int counter = 0 ; 
+List <T> list ;
+public myStack () {
+   list = new List<T>();
+}
 
+public void push( T element ){
+  list.Add(element);
+  counter++;
+
+}
 
 
 
